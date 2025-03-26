c37f23c0c2b0463b961d26c7d16859c0f6f8e4d6
luismidv <perezmartluismiguel@gmail.com>
2025-03-26 10:18:14 +0100

    New

--- src/Room.jsx
+++ src/Room.jsx
@@ -91,7 +91,7 @@ export function RoomBannerTenants({label, img, rooms, metters, bathrooms,price, 
     bathroomsvg, eurosvg,profilesvg, username, directionsvg, direction, description}) {
     
     return(
-        <div className = "room-label lg:w-[800px] lg:h-[500px] md:w-[800px] md:h-[500px] w-[500px] h-[100px] left-[-120px] top-[-100px] lg:left-[300px] md:left-[300px] md:top-[100px] mb-300 md:mb-0">
+        <div className = "room-label lg:w-[800px] lg:h-[500px] md:w-[800px] md:h-[500px] w-[500px] h-[100px] left-[60px] top-[-100px] lg:left-[300px] md:left-[300px] md:top-[100px] mb-300 md:mb-0">
             <img className = "room-banner-img w-[80%] height-[70%] relative top-[550px] left-[0px] md:top-[0px] md:left-[420px] md:w-[50%] md:h-[100%]" src = {img}></img>
             <p className = "desc-p"> {label}  </p>
             <div className = "head-info absolute md:left-[120px] md:top-[80px] left-[265px] top-[85px]">
