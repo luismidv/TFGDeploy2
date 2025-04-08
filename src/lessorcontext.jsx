e0b77175aeced67711ed3af0404e990db9029275
luismidv <perezmartluismiguel@gmail.com>
2025-04-08 18:53:31 +0200

    Finished tech test

--- src/lessorcontext.jsx
+++ src/lessorcontext.jsx
@@ -2,13 +2,33 @@ import React, { createContext, useState, useContext } from "react";
 
 const LessorContext = createContext();
 export const LessorProvider = ({children}) => {
-    const [lessorData, setLessorData] = useState(null);
-    const [lessorId, setLessorId] = useState(null);
+    const [lessorData, setLessorData] = useState(() => {
+        const savedData = localStorage.getItem("lessorData");
+        return savedData ? JSON.parse(savedData) : null;
+    });
+    
+    const [lessorId, setLessorId] = useState(() => {
+        return localStorage.getItem("lessorId") || null;
+    });
+    useEffect(() => {
+        if (lessorId) {
+            localStorage.setItem("lessorId", lessorId);
+        } else {
+            localStorage.removeItem("lessorId");
+        }
+    }, [lessorId]);
 
+    useEffect(() => {
+        if (lessorData) {
+            localStorage.setItem("lessorData", JSON.stringify(lessorData));
+        } else {
+            localStorage.removeItem("lessorData");
+        }
+    }, [lessorData]);
+    
     return (
         <LessorContext.Provider value = {{lessorData, setLessorData, lessorId, setLessorId}}>
         {children}
         </LessorContext.Provider>
-    )
-}
+    )}
 export const useLessor = () => useContext(LessorContext)
