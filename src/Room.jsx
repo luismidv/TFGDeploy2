1554389638d676ffad1f16ec29ead487fb81ec5d
luismidv <perezmartluismiguel@gmail.com>
2025-05-03 10:47:08 +0200

    push

--- src/Room.jsx
+++ src/Room.jsx
@@ -171,7 +171,7 @@ export function RoomInfo({label, img, rooms, metters, bathrooms,price, bedroomsv
             if (tenantData) {
                 try {
                     console.log("Tenant data: ", tenantData)
-                    const formattedTenants = Object.keys(tenantData.names).map((key, index) => ({
+                    const formattedTenants = tenantData.names.map((_, index) => ({
                         names: tenantData.names[index],
                         age: tenantData.age[index],
                         smoking: tenantData.smoking[index] === "Yes" ? "Smokes" : "No smoking",
